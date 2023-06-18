a9f92c4d1b173205b949f87c2fcdeca42889b5ae
shohsultoncode <shohsulton.3039@gmail.com>
2023-06-18 22:05:34 +0500

    .

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -9,13 +9,19 @@ export class CreateUserDto {
 
     @IsString()
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
     user_last_name: string;
 
     @IsString()
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
     user_username: string;
 
     @IsString()
     @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
     user_password: string;
 }
